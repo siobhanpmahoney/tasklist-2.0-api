b935dad78a96105ee3dff2f7504e419ca7d703da
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-05-19 09:54:57 -0400

    working on layout stuff

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -18,7 +18,8 @@ class Api::V1::TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
-    render json: @task
+    task_info = @task.task_info
+    render json: task_info
   end
 
   def update
